ba414099e9bf5aa3b1062b4ce9359c9bb27f451e
mike mao <mikemaocn@hotmail.com>
2017-12-29 11:24:01 +0800

    Commit 2

--- HelloWorld/HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld/HelloWorld.py
@@ -3,3 +3,12 @@ score = [85,79,93]
 print("语文成绩: %d 分" % score[0])
 print("数学成绩: %d 分" % score[1])
 print("英语成绩: %d 分" % score[2])
+
+import tensorflow as tf
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
+
+a = tf.constant(10)
+b = tf.constant(32)
+print(sess.run(a+b))
